2b844557c0e64f586c332d37ebb51c87b7852d48
Mario Rizki <mariorizki07@gmail.com>
2023-01-05 03:55:10 +0700

    change promt statement

--- pages/api/gifts.js
+++ pages/api/gifts.js
@@ -17,5 +17,5 @@ export default async function (req, res) {
 }
 
 function generatePrompt(priceMin, priceMax, gender, age, hobbies) {
-  return `suggest 3 Christmas gift ideas between ${priceMin}$ and ${priceMax}$ for a ${age} years old ${gender} that is into ${hobbies}.`;
+  return `suggest 3 gift ideas between ${priceMin}$ and ${priceMax}$ for a ${age} years old ${gender} that is into ${hobbies}.`;
 }
